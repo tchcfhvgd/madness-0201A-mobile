0568bec39e7ac343338719d2b311a7756ef4182a
tchcfhvgd <99186628+tchcfhvgd@users.noreply.github.com>
2024-10-02 06:31:20 +0800

    Update Controls.hx

--- source/backend/Controls.hx
+++ source/backend/Controls.hx
@@ -34,7 +34,6 @@ class Controls
 	public var NOTE_DOWN_P(get, never):Bool;
 	public var NOTE_LEFT_P(get, never):Bool;
 	public var NOTE_RIGHT_P(get, never):Bool;
-	public var SPACE_P(get, never):Bool;
 	private function get_UI_UP_P() return justPressed('ui_up');
 	private function get_UI_DOWN_P() return justPressed('ui_down');
 	private function get_UI_LEFT_P() return justPressed('ui_left');
@@ -43,7 +42,6 @@ class Controls
 	private function get_NOTE_DOWN_P() return justPressed('note_down');
 	private function get_NOTE_LEFT_P() return justPressed('note_left');
 	private function get_NOTE_RIGHT_P() return justPressed('note_right');
-    private function get_SPACE_P() return justPressed('space');
     
 	// Held buttons (directions)
 	public var UI_UP(get, never):Bool;
@@ -54,7 +52,6 @@ class Controls
 	public var NOTE_DOWN(get, never):Bool;
 	public var NOTE_LEFT(get, never):Bool;
 	public var NOTE_RIGHT(get, never):Bool;
-	public var SPACE(get, never):Bool;
 	private function get_UI_UP() return pressed('ui_up');
 	private function get_UI_DOWN() return pressed('ui_down');
 	private function get_UI_LEFT() return pressed('ui_left');
@@ -63,7 +60,6 @@ class Controls
 	private function get_NOTE_DOWN() return pressed('note_down');
 	private function get_NOTE_LEFT() return pressed('note_left');
 	private function get_NOTE_RIGHT() return pressed('note_right');
-    private function get_SPACE() return pressed('space');
     
 	// Released buttons (directions)
 	public var UI_UP_R(get, never):Bool;
@@ -74,7 +70,6 @@ class Controls
 	public var NOTE_DOWN_R(get, never):Bool;
 	public var NOTE_LEFT_R(get, never):Bool;
 	public var NOTE_RIGHT_R(get, never):Bool;
-	public var SPACE_R(get, never):Bool;
 	private function get_UI_UP_R() return justReleased('ui_up');
 	private function get_UI_DOWN_R() return justReleased('ui_down');
 	private function get_UI_LEFT_R() return justReleased('ui_left');
@@ -83,7 +78,6 @@ class Controls
 	private function get_NOTE_DOWN_R() return justReleased('note_down');
 	private function get_NOTE_LEFT_R() return justReleased('note_left');
 	private function get_NOTE_RIGHT_R() return justReleased('note_right');
-	private function get_SPACE_R() return justReleased('space');
 
 
 	// Pressed buttons (others)
@@ -113,7 +107,7 @@ class Controls
 		controllerMode = false;
 		}				
 
-		return result || _myGamepadJustPressed(gamepadBinds[key]) == true #if android || checkAndroidControl_justPressed(key) == true #end;
+		return result || _myGamepadJustPressed(gamepadBinds[key]) == true #if ios || checkAndroidControl_justPressed(key) == true #end;
 	}
 
 	public function pressed(key:String)
@@ -128,7 +122,7 @@ class Controls
 		controllerMode = false;
 		}
 
-		return result || _myGamepadPressed(gamepadBinds[key]) == true #if android || checkAndroidControl_pressed(key) == true #end;
+		return result || _myGamepadPressed(gamepadBinds[key]) == true #if ios || checkAndroidControl_pressed(key) == true #end;
 	}
 
 	public function justReleased(key:String)
@@ -143,7 +137,7 @@ class Controls
 		controllerMode = false;
 		}
 
-		return result || _myGamepadJustReleased(gamepadBinds[key]) == true #if android || checkAndroidControl_justReleased(key) == true #end;
+		return result || _myGamepadJustReleased(gamepadBinds[key]) == true #if ios || checkAndroidControl_justReleased(key) == true #end;
 	}
 
 	public var controllerMode:Bool = false;
