583e13be15d2edbba416ce416dbdd532a332a674
tchcfhvgd <99186628+tchcfhvgd@users.noreply.github.com>
2024-10-02 06:40:26 +0800

    Update NGIntro.hx

--- source/NGIntro.hx
+++ source/NGIntro.hx
@@ -9,6 +9,11 @@ class NGIntro extends FlxState
     var snd:FlxSound;
     override function create() 
     {
+        #if mobile
+	    noCheckPress();
+        removeVirtualPad();
+	    #end
+        
         FlxG.mouse.visible = false;
         Main.fpsVar.visible = false; 
         FlxG.camera.bgColor = FlxColor.BLACK;
